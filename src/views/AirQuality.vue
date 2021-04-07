03f4f5dce1eee56a8a8ce41e2823a07932119e0d
Cassandra <cdhm99@gmail.com>
2021-04-07 14:41:15 -0400

    final version working code

--- src/views/AirQuality.vue
+++ src/views/AirQuality.vue
@@ -1,28 +1,14 @@
+<!--
 <template>
     <div class="airQuality">
         <h2 class="text-center">Air Pollution Risk</h2>
-      
-      <!-- <div class="columns medium-4" > -->
-        <!-- <h3>Current Air Quality by ZIP</h3> -->
          <h3>Current Air Quality for ZIP Code "{{zipcodeComputed}}"</h3>
-         <!-- cass did above -->
-        
-        <!-- <textarea v-model="airDataString" rows="50" cols="50" autofocus>
-
-        </textarea>
-
-        <ul>
-          <li v-for="{stations} in parameters" v-bind:key="stations">
-            <p>{{ stations }}</p>
-          </li>
-        </ul> -->
 
 <h4 style="margin-left: 10px; padding-left: 10px;">Current Air Quality for {{airQuality.stations[0].placeName}} {{airQuality.stations[0].postalCode}}, {{airQuality.stations[0].state}}</h4>
       <p style="margin-left: 10px; padding-left: 10px;">Last updated at {{airQuality.stations[0].updatedAt}}</p>
 
       
       <div class="columns medium-6" >       
-        <!---- <textarea v-model="airDataString" rows="50" cols="50" autofocus></textarea> -->
         <ul>
           <li v-for="(item, index) in airQuality.stations" v-bind:key="index">
             <h4>Pollution Measurements</h4>
@@ -43,8 +29,6 @@
 
       <div class="columns medium-6" >
         <h4>Fire Hazard</h4>
-        <!-- <textarea v-model="fireDataString" rows="50" cols="50" autofocus></textarea> -->
-
 
         <ul>
           <li v-for="(item, index) in fireHazard.data" v-bind:key="index">
@@ -58,10 +42,6 @@
         </ul>
         
       </div>
-
-
-
-      <!-- </div> -->
      
     </div>
 </template>
@@ -70,13 +50,13 @@
 
 export default {
     name: 'AirQuality',
-    props: ['zip'], //cass did
+    props: ['zip'], 
 
     data() {
         return {
             airDataString: String,
             airQuality: [],
-            zipcode: this.$route.params.id, //cass did
+            zipcode: this.$route.params.id, 
             fireDataString: String,
             fireHazard: []
         };
@@ -89,7 +69,7 @@ export default {
     },
 
     computed: {
-        zipcodeComputed() { //cass did
+        zipcodeComputed() { 
             return this.$route.query.zip;
         },
         parameters(){
@@ -106,7 +86,6 @@ export default {
         this.$axios.request(optionsair).then(response => {
             this.airDataString = JSON.stringify(response.data, null, "\t");
             this.airQuality = response.data;
-            // console.log(this.listDataString);
             return response; 
         }),
 
@@ -115,27 +94,24 @@ export default {
             this.fireHazard = response.data;
             console.log(this.listDataString);
             return response; 
-            // this.results = response.data;
-            // console.log("something");
         })
     }
 };
 
 const apikey = 'N4rk05Lsxs6pG7FfzqYl18newiD5aJZB6zReT8Pn';
-//const url =  'https://api.ambeedata.com/latest/by-country-code?countryCode=IN'
 
 	
 var optionsair = {
   method: 'GET',
   url: 'https://api.ambeedata.com/latest/by-postal-code',
-  params: {postalCode: '90250', countryCode: 'US'}, //postal code 90250
+  params: {postalCode: '90250', countryCode: 'US'}, 
   headers: {'x-api-key': apikey, 'Content-type': 'application/json'}
 };
 
 var optionsfire = {
     method: 'GET',
     url: 'https://api.ambeedata.com/latest/fire',
-    params: {lat: '34.0522', lng: '118.2437'}, // LA Lat and Long for testing
+    params: {lat: '34.0522', lng: '118.2437'}, 
     headers: {'x-api-key': apikey, 'Content-type': 'application/json'}
   };
 </script>
@@ -148,5 +124,155 @@ var optionsfire = {
 .airQuality h3 {
     font-family: Verdana;
     color: #e6f2ff;
+}
+</style>
+-->
+
+<template>
+  <div class="airQuality">
+    <h2 class="text-center">Air Pollution Risk</h2>
+
+    <!-- <div class="columns medium-4" > -->
+    <!-- <h3>Current Air Quality by ZIP</h3> -->
+    <h3>Current Air Quality for ZIP Code "{{ zipcodeComputed }}"</h3>
+    <!-- cass did above -->
+
+    <!-- <textarea v-model="airDataString" rows="50" cols="50" autofocus>
+
+        </textarea>
+
+        <ul>
+          <li v-for="{stations} in parameters" v-bind:key="stations">
+            <p>{{ stations }}</p>
+          </li>
+        </ul> -->
+
+    <h4 style="margin-left: 10px; padding-left: 10px;">
+      Current Air Quality for {{ airQuality.stations[0].placeName }}
+      {{ airQuality.stations[0].postalCode }},
+      {{ airQuality.stations[0].state }}
+    </h4>
+    <p style="margin-left: 10px; padding-left: 10px;">
+      Last updated at {{ airQuality.stations[0].updatedAt }}
+    </p>
+
+    <div class="columns medium-6">
+      <!---- <textarea v-model="airDataString" rows="50" cols="50" autofocus></textarea> -->
+      <ul>
+        <li v-for="(item, index) in airQuality.stations" v-bind:key="index">
+          <h4>Pollution Measurements</h4>
+          <p>CO: {{ item.CO }}</p>
+          <p>NO2: {{ item.NO2 }}</p>
+          <p>OZONE: {{ item.OZONE }}</p>
+          <p>PM10: {{ item.PM10 }}</p>
+          <p>PM25: {{ item.PM25 }}</p>
+          <p>SO2: {{ item.SO2 }}</p>
+          <h4>Air Quality</h4>
+          <p>Air Quality Index: {{ item.AQI }}</p>
+          <p>Particulant Matter: {{ item.aqiInfo.pollutant }}</p>
+          <p>PM Concentration: {{ item.aqiInfo.concentration }}</p>
+          <p>AQI Category: {{ item.aqiInfo.category }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <div class="columns medium-6">
+      <h4>Fire Hazard</h4>
+      <!-- <textarea v-model="fireDataString" rows="50" cols="50" autofocus></textarea> -->
+
+      <ul>
+        <li v-for="(item, index) in fireHazard.data" v-bind:key="index">
+          <p>Latitude: {{ item.lat }}</p>
+          <p>Longitude: {{ item.lng }}</p>
+          <p>Confidence: {{ item.confidence }}</p>
+          <p>FRP: {{ item.frp }}</p>
+          <p>Detection time: {{ item.detection_time }}</p>
+          <p>Distance: {{ item.distance }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AirQuality",
+  props: ["zip"], //cass did
+
+  data() {
+    return {
+      airDataString: String,
+      airQuality: [],
+      zipcode: this.$route.params.id, //cass did
+      fireDataString: String,
+      fireHazard: [],
+    };
+  },
+
+  methods: {
+    containsKey(obj, key) {
+      return Object.keys(obj).includes(key);
+    },
+  },
+
+  computed: {
+    zipcodeComputed() {
+      //cass did
+      return this.$route.query.zip;
+    },
+    parameters() {
+      return Object.values(this.airQuality).map(({ stations }) => ({
+        stations,
+      }));
+    },
+  },
+
+  mounted() {
+    var optionsair = {
+      method: "GET",
+      url: "https://api.ambeedata.com/latest/by-postal-code",
+      params: { postalCode: this.$route.query.zip, countryCode: "US" }, //postal code 90250
+      headers: { "x-api-key": apikey, "Content-type": "application/json" },
+    };
+
+    var optionsfire = {
+      method: "GET",
+      url: "https://api.ambeedata.com/latest/fire",
+      params: { lat: "34.0522", lng: "118.2437" }, // LA Lat and Long for testing
+      headers: { "x-api-key": apikey, "Content-type": "application/json" },
+    };
+
+    this.$axios.request(optionsair).then((response) => {
+      this.airDataString = JSON.stringify(response.data, null, "\t");
+      this.airQuality = response.data;
+      // console.log(this.listDataString);
+      return response;
+    }),
+      this.$axios.request(optionsfire).then((response) => {
+        this.fireDataString = JSON.stringify(response.data, null, "\t");
+        this.fireHazard = response.data;
+        console.log(this.listDataString);
+        return response;
+        // this.results = response.data;
+        // console.log("something");
+      });
+  },
+};
+
+//const apikey = 'N4rk05Lsxs6pG7FfzqYl18newiD5aJZB6zReT8Pn'; //derya apikey
+const apikey = "oLCKWMNsb84Z7k1RHAoP1TjSOniPFvy61mJfUt23"; //deniz apikey
+//const url =  'https://api.ambeedata.com/latest/by-country-code?countryCode=IN'
+</script>
+
+<style scoped>
+.airQuality {
+  color: #cce5ff;
+  font-family: Verdana;
+}
+.airQuality h3 {
+  font-family: Verdana;
+  color: #e6f2ff;
 }
 </style>
--- src/views/Alerts.vue
+++ src/views/Alerts.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
     <div class="alertsPage">
         <h2>National Weather Alerts</h2>
@@ -9,17 +10,10 @@
                 <select @change="fetchAPIData()" class="form-control">
                     <option v-for="(stateOpt, indx) in states" :value="stateOpt" v-bind:key="indx" 
                     >
-                    <!-- v-on:change="fetchAPIData()" -->
-                    <!-- v-on:click="fetchAPIData()" -->
-                    <!-- v-on:select="fetchAPIData()" -->
-                    <!-- v-on:click="setState(stateOpt)" -->
-                    <!-- v-bind:placeholder="fetchAPIData()" -->
-                    <!-- {{ stateOpt }} -->
                     </option>
                 </select>
             </datalist>
             </label>
-            <!-- <p>{{stateSelected}}</p> -->
         </div>
 
         <h3>Showing alerts for {{selected}}</h3>
@@ -34,12 +28,11 @@
 <script>
 
 export default {
-    name: 'Alerts', //cass added
+    name: 'Alerts', 
     data() {
         return {
             responseAvailable: false,
             apiKey: '<YOUR_RAPIDAPI_KEY>',
-           // selectedState: "CA", //cass note to self need to have this start out blank
             states : [ "AK",
                         "AL",
                         "AR",
@@ -95,31 +88,16 @@ export default {
                         "WI",
                         "WV",
                         "WY"],
-                    selected: '', //cass added
+                    selected: '', 
                     result: ''
         }
     },
-
-// cass added
 computed: {
         stateSelected() {
-            // console.log(this.selected);
-            // return this.states.find( i => i.value == this.selected );
             return this.selected;
         }
 
-
-        // setState(item) {
-        //     console.log(item); //cass debug
-        //     this.selectedState = item;
-        //     //console.log(this.selectedState); //cass debug
-        //     this.fetchAPIData();
-        //     return item;
-        // }
-
-    },
-
-    
+    }, 
 
     methods: {
 
@@ -128,7 +106,6 @@ computed: {
         },
 
         fetchAPIData( ) { 
-            // console.log("YOU MADE IT");
             this.responseAvailable = false;
             fetch("https://api.weather.gov/alerts?status=actual&message_type=alert&region_type=land&severity=severe", {
                 "method": "GET",
@@ -143,7 +120,6 @@ computed: {
                 }                 
             })
             .then(response => {
-                //console.log("Response alert");
                 var result = [];
                 var c = 0;
                 for (var i = 0; i < response.features.length; i++) {
@@ -156,20 +132,12 @@ computed: {
                     result[0] = "No alerts to show at the moment";
                 }
                 this.result = result;
-                //console.log(this.result.length);
-                //this.result = response.features; 
                 this.responseAvailable = true;
             })
             .catch(err => {
                 console.log(err);
             });
         },
-        // setState(item) {
-        //     //console.log(item); //cass debug
-        //     this.selectedState = item;
-        //     console.log(this.selectedState); //cass debug
-        //    this.fetchAPIData(); //cass removed
-        // }
     },
     beforeMount(){
         this.fetchAPIData()
@@ -177,14 +145,6 @@ computed: {
   };
 
 </script>
-
-<!-- Deniz's code
-    <script src="https://unpkg.com/vue"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
--->
 
 <style scoped>
 
@@ -225,4 +185,248 @@ computed: {
         font-family: Calibri Light, Verdana, Consolas, Papyrus, Avenir, Helvetica, Arial, sans-serif;
     }
 
+</style>
+-->
+
+<template>
+  <div class="alertsPage">
+    <h2>National Weather Alerts</h2>
+
+    <div id="statesSelection">
+      <label>
+        Select state:<br />
+        <input
+          type="text"
+          v-model="selected"
+          v-on:input="fetchAPIData"
+          list="state"
+        />
+        <datalist id="state">
+          <select @change="fetchAPIData()" class="form-control">
+            <option
+              v-for="(stateOpt, indx) in states"
+              :value="stateOpt"
+              v-bind:key="indx"
+            >
+              <!-- v-on:change="fetchAPIData()" -->
+              <!-- v-on:click="fetchAPIData()" -->
+              <!-- v-on:select="fetchAPIData()" -->
+              <!-- v-on:click="setState(stateOpt)" -->
+              <!-- v-bind:placeholder="fetchAPIData()" -->
+              <!-- {{ stateOpt }} -->
+            </option>
+          </select>
+        </datalist>
+      </label>
+      <!-- <p>{{stateSelected}}</p> -->
+    </div>
+
+    <h3>Showing alerts for {{ selected }}</h3>
+    <ul class="alertsUl">
+      <li class="alertsLi" v-for="(item, index) in result" v-bind:key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alerts", //cass added
+  data() {
+    return {
+      responseAvailable: false,
+      apiKey: "<YOUR_RAPIDAPI_KEY>",
+      //selectedState: "CA", //cass note to self need to have this start out blank
+      states: [
+        "AK",
+        "AL",
+        "AR",
+        "AS",
+        "AZ",
+        "CA",
+        "CO",
+        "CT",
+        "DC",
+        "DE",
+        "FL",
+        "GA",
+        "GU",
+        "HI",
+        "IA",
+        "ID",
+        "IL",
+        "IN",
+        "KS",
+        "KY",
+        "LA",
+        "MA",
+        "MD",
+        "ME",
+        "MI",
+        "MN",
+        "MO",
+        "MS",
+        "MT",
+        "NC",
+        "ND",
+        "NE",
+        "NH",
+        "NJ",
+        "NM",
+        "NV",
+        "NY",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "PR",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VA",
+        "VI",
+        "VT",
+        "WA",
+        "WI",
+        "WV",
+        "WY",
+      ],
+      selected: "", //cass added
+      result: "",
+    };
+  },
+
+  // cass added
+  computed: {
+    stateSelected() {
+      //console.log("Changed state:" + this.selected);
+      // return this.states.find( i => i.value == this.selected );
+      //this.fetchAPIData();
+      return this.selected;
+    },
+
+    // setState(item) {
+    //     console.log(item); //cass debug
+    //     this.selectedState = item;
+    //     //console.log(this.selectedState); //cass debug
+    //     this.fetchAPIData();
+    //     return item;
+    // }
+  },
+
+  methods: {
+    plzWork() {
+      console.log("cool");
+    },
+
+    fetchAPIData() {
+      // console.log("YOU MADE IT");
+      this.responseAvailable = false;
+      fetch(
+        "https://api.weather.gov/alerts?status=actual&message_type=alert&region_type=land&severity=severe",
+        {
+          method: "GET",
+          headers: {},
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            alert(
+              "Server returned " + response.status + " : " + response.statusText
+            );
+          }
+        })
+        .then((response) => {
+          //console.log("Response alert");
+          var result = [];
+          var c = 0;
+          for (var i = 0; i < response.features.length; i++) {
+            if (
+              response.features[i].properties.senderName.includes(this.selected)
+            ) {
+              //was selectedState
+              result[c] = JSON.stringify(
+                response.features[i].properties.headline
+              );
+              c++;
+            }
+          }
+          if (result.length == 0) {
+            result[0] = "No alerts to show at the moment";
+          }
+          this.result = result;
+          //console.log(this.result.length);
+          //this.result = response.features;
+          this.responseAvailable = true;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    setState(item) {
+      console.log(item); //cass debug
+      this.selected = item;
+      console.log(this.selectedState); //cass debug
+      this.fetchAPIData(); //cass removed
+    },
+  },
+  beforeMount() {
+    this.fetchAPIData();
+  },
+};
+</script>
+
+<!-- Deniz's code
+    <script src="https://unpkg.com/vue"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+-->
+
+<style scoped>
+.alertsPage {
+  font-family: Verdana, Calibri Light, Consolas, Papyrus, Avenir, Helvetica,
+    Arial, sans-serif;
+}
+
+.alertsPage h2 {
+  color: #cce5ff;
+}
+
+.alertsPage h3 {
+  color: #e6f2ff;
+}
+
+.alertsPage label {
+  color: #e6f2ff;
+}
+
+.alertsUl {
+  counter-reset: gradient-counter;
+  list-style: none;
+  margin-left: 5%;
+  margin-right: 5%;
+  padding-left: 0;
+  padding-right: 0;
+}
+
+.alertsLi {
+  border: solid 1px #cce5ff;
+  padding: 5px;
+  margin: 5px;
+  background-color: #e6f2ff;
+  position: relative;
+  color: #1a89ff;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: Calibri Light, Verdana, Consolas, Papyrus, Avenir, Helvetica,
+    Arial, sans-serif;
+}
 </style>
